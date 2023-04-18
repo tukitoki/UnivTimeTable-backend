2ad05032628638b2f606761ca7b9d7e3922c6341
tukitoki <tokichiihere@gmail.com>
2023-04-18 20:18:16 +0300

    first commit

---
+++ src/main/java/ru/vsu/cs/timetable/univtimetablebackend/UnivTimeTableBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.timetable.univtimetablebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnivTimeTableBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnivTimeTableBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vsu/cs/timetable/univtimetablebackend/UnivTimeTableBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.timetable.univtimetablebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnivTimeTableBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
