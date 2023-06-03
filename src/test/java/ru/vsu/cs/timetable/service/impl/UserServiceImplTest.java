764eb62c6265ac24a5322bd395b72ac4422f11da
FedotBegemot <fedorsviridov@gmail.com>
2023-06-04 02:09:50 +0300

    get by ID, email and username tests

--- src/test/java/ru/vsu/cs/timetable/service/impl/UserServiceImplTest.java
+++ src/test/java/ru/vsu/cs/timetable/service/impl/UserServiceImplTest.java
@@ -4,13 +4,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.stubbing.OngoingStubbing;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.MockMvc;
-import ru.vsu.cs.timetable.controller.UserController;
-import ru.vsu.cs.timetable.dto.user.UserDto;
+import ru.vsu.cs.timetable.entity.Group;
 import ru.vsu.cs.timetable.entity.User;
 import ru.vsu.cs.timetable.entity.enums.UserRole;
 import ru.vsu.cs.timetable.repository.UserRepository;
@@ -18,34 +15,33 @@ import ru.vsu.cs.timetable.repository.UserRepository;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
 
-/*
-    @Autowired
-    private MockMvc mockMvc;
-*/
-
-    @Mock
-    private UserServiceImpl userServiceImpl;
-
     @Mock
     private UserRepository userRepository;
-
     @Mock
-    private UserController userController;
-
+    private User user1;
     @Mock
-    private UserDto userDto;
+    private Group group1;
+    @InjectMocks
+    private UserServiceImpl userServiceImpl;
 
     @BeforeEach
     void setUp() {
-        userDto = new UserDto(1213113421L, "HEADMAN", "Иванов Иван Иванович", "ivan", "ivan@mail.ru",
-                "password","Воронеж", null, null, null);
-        userServiceImpl.createUser(userDto);
+        group1.setId(1L);
+        group1.setGroupNumber(1);
+        group1.setStudentsAmount(1);
+        user1.setCity("Воронеж");
+        user1.setEmail("ivan@mail.ru");
+        user1.setFullName("Иванов Иван Иванович");
+        user1.setId(121311342L);
+        user1.setUsername("ivan");
+        user1.setRole(UserRole.valueOf("HEADMAN"));
+        user1.setPassword("password");
+        user1.setGroup(group1);
     }
 
     @AfterEach
@@ -74,18 +70,49 @@ class UserServiceImplTest {
 
     @Test
     void deleteUser() {
+//        when(userRepository.delete(user1).thenReturn);
+
+//        User userToCompare = userServiceImpl.deleteUser(1213113421L);
+//        userServiceImpl.deleteUser(1L);
+
+//        assert(group1.getStudentsAmount().equals(0));
+//        assert(user1.());
+
+////        int rowCount = Ebean.find(User.class).where().eq("id", 1).findRowCount();
+
     }
 
     @Test
     void getUserById() {
+        when(userRepository.findById(121311342L))
+                .thenReturn(Optional.of(user1));
 
+        User userToCompare = userServiceImpl.getUserById(121311342L);
+
+        assert(userToCompare.equals(user1));
+        assertThat(user1.getId()).isNotNull();
+//        userServiceImpl.createUser(userDto);
+//        Optional<User> user2 = userRepository.findById(1213113421L);
     }
 
     @Test
     void getUserByUsername() {
+        when(userRepository.findByUsername("ivan"))
+                .thenReturn(Optional.of(user1));
+
+        User userToCompare = userServiceImpl.getUserByUsername("ivan");
+
+        assert(userToCompare.equals(user1));
     }
 
     @Test
     void getUserByEmail() {
+        when(userRepository.findByEmail("ivan@mail.ru"))
+                .thenReturn(Optional.of(user1));
+
+        User userToCompare = userServiceImpl.getUserByEmail("ivan@mail.ru");
+
+        assert(userToCompare.equals(user1));
     }
 }
+
