bf9b7ed2243766987980c2f5afc8b433210aae5d
FedotBegemot <fedorsviridov@gmail.com>
2023-06-05 13:11:46 +0300

    all tests for UserServiceImpl done

--- src/test/java/ru/vsu/cs/timetable/service/impl/UserServiceImplTest.java
+++ src/test/java/ru/vsu/cs/timetable/service/impl/UserServiceImplTest.java
@@ -1,6 +1,5 @@
 package ru.vsu.cs.timetable.service.impl;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,6 +8,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.vsu.cs.timetable.dto.user.UserDto;
 import ru.vsu.cs.timetable.entity.Faculty;
 import ru.vsu.cs.timetable.entity.Group;
@@ -17,8 +17,10 @@ import ru.vsu.cs.timetable.entity.User;
 import ru.vsu.cs.timetable.entity.enums.UserRole;
 import ru.vsu.cs.timetable.mapper.UserMapper;
 import ru.vsu.cs.timetable.repository.GroupRepository;
-import ru.vsu.cs.timetable.repository.UniversityRepository;
 import ru.vsu.cs.timetable.repository.UserRepository;
+import ru.vsu.cs.timetable.service.FacultyService;
+import ru.vsu.cs.timetable.service.GroupService;
+import ru.vsu.cs.timetable.service.UniversityService;
 
 import java.util.Optional;
 
@@ -34,17 +36,25 @@ class UserServiceImplTest {
     @Mock
     private GroupRepository groupRepository;
     @Mock
-    private UniversityRepository universityRepository;
-    @Mock
-    private UserMapper userMapper = new UserMapper();
+    private UserMapper userMapper;
     @InjectMocks
     private UserServiceImpl userServiceImpl;
+    @Mock
+    private UniversityService universityService;
+    @Mock
+    private GroupService groupService;
+    @Mock
+    private FacultyService facultyService;
+    @Mock
+    private PasswordEncoder passwordEncoder;
     private final UserDto userDto = new UserDto(121311342L, "HEADMAN", "Иванов Иван Иванович", "ivan", "ivan@mail.ru",
-                                                "password","Воронеж", null, null, null);
+            "Воронеж", "password", null, null, null);
     private Group group = new Group();
     private User user = new User();
-    private University university = new University();
-    private Faculty faculty = new Faculty();
+    private University universityVSU = new University();
+    private Faculty facultyFKN = new Faculty();
+    private Faculty facultyPMM = new Faculty();
+
 
     @BeforeEach
     void setUp() {
@@ -60,10 +70,19 @@ class UserServiceImplTest {
         user.setRole(UserRole.valueOf("HEADMAN"));
         user.setPassword("password");
         user.setGroup(group);
-    }
 
-    @AfterEach
-    void tearDown() {}
+        universityVSU.setId(1L);
+        universityVSU.setCity("Воронеж");
+        universityVSU.setName("ВГУ");
+
+        facultyFKN.setId(1L);
+        facultyFKN.setName("ФКН");
+        facultyFKN.setUniversity(universityVSU);
+
+        facultyPMM.setId(2L);
+        facultyPMM.setName("ПММ");
+        facultyPMM.setUniversity(universityVSU);
+    }
 
     @Test
     void getUserDtoById() {
@@ -81,19 +100,98 @@ class UserServiceImplTest {
 
     @Test
     void createUser() {
+        UserDto userToCreateDto = new UserDto(4L, "HEADMAN", "Семёнов Артём Валерьевич", "Artem", "artem@mail.ru", "Воронеж",
+                "password", 1L, 1L, 1L);
+
+        when(groupService.findGroupById(1L)).
+                thenReturn(group);
+        when(facultyService.findFacultyById(1L)).
+                thenReturn(facultyFKN);
+        when(universityService.findUnivById(1L)).
+                thenReturn(universityVSU);
+        when(passwordEncoder.encode("password")).
+                thenReturn("password");
+
+        User userToCreate = new User();
+        userToCreate.setCity("Воронеж");
+        userToCreate.setEmail("artem@mail.ru");
+        userToCreate.setFullName("Семёнов Артём Валерьевич");
+        userToCreate.setId(4L);
+        userToCreate.setUsername("Artem");
+        userToCreate.setRole(UserRole.valueOf("HEADMAN"));
+        userToCreate.setPassword("password");
+        userToCreate.setFaculty(facultyFKN);
+        userToCreate.setGroup(group);
+        userToCreate.setUniversity(universityVSU);
+
+        when(userMapper.toEntity(userToCreateDto, universityVSU, group, facultyFKN, userToCreate.getPassword())).
+                thenReturn(userToCreate);
+        when(userRepository.save(userToCreate)).thenReturn(userToCreate);
+
+        userServiceImpl.createUser(userToCreateDto);
+
+        assert(userToCreate.getFullName().equals("Семёнов Артём Валерьевич"));
+        assert(userToCreate.getEmail().equals("artem@mail.ru"));
+        assert(userToCreate.getUsername().equals("Artem"));
+        assert(userToCreate.getFaculty().getName().equals("ФКН"));
     }
 
     @Test
     void updateUser() {
-//        when(userRepository.findById(121311342L))
-//                .thenReturn(Optional.of(user));
-//        when(userRepository.findByUsername("ivan"))
-//                .thenReturn(Optional.of(user));
-//        when(userRepository.findByEmail("ivan@mail.ru"))
-//                .thenReturn(Optional.of(user));
-//
-//        when(universityRepository.findById(userDto.getUniversityId()).thenReturn(Optional.of(user.getUniversity())));
-//        when(groupRepository.findById(userDto.getGroupId());
+        User userToUpdate = new User();
+        userToUpdate.setCity("Воронеж");
+        userToUpdate.setEmail("petraf@mail.ru");
+        userToUpdate.setFullName("Петров Пётр Петрович");
+        userToUpdate.setId(3L);
+        userToUpdate.setUsername("Petro");
+        userToUpdate.setRole(UserRole.valueOf("HEADMAN"));
+        userToUpdate.setPassword("password");
+        userToUpdate.setFaculty(facultyPMM);
+        userToUpdate.setGroup(group);
+
+        assert(userToUpdate.getFullName().equals("Петров Пётр Петрович"));
+        assert(userToUpdate.getEmail().equals("petraf@mail.ru"));
+        assert(userToUpdate.getUsername().equals("Petro"));
+        assert(userToUpdate.getFaculty().getName().equals("ПММ"));
+
+        UserDto newUserDto = new UserDto(3L, "HEADMAN", "Семёнов Артём Валерьевич", "semen", "semen@mail.ru", "Воронеж",
+                "password", 1L, 1L, 1L);
+        User newUser = new User();
+        newUser.setCity("Воронеж");
+        newUser.setEmail("semen@mail.ru");
+        newUser.setFullName("Семёнов Артём Валерьевич");
+        newUser.setId(3L);
+        newUser.setUsername("semen");
+        newUser.setRole(UserRole.valueOf("HEADMAN"));
+        newUser.setPassword("password");
+        newUser.setFaculty(facultyFKN);
+        newUser.setGroup(group);
+
+        when(userRepository.findById(3L))
+                .thenReturn(Optional.of(userToUpdate));
+        when(userRepository.findByUsername("semen"))
+                .thenReturn(Optional.of(userToUpdate));
+        when(userRepository.findByEmail("semen@mail.ru"))
+                .thenReturn(Optional.of(userToUpdate));
+
+        when(groupService.findGroupById(1L)).
+                thenReturn(group);
+        when(facultyService.findFacultyById(1L)).
+                thenReturn(facultyFKN);
+        when(universityService.findUnivById(1L)).
+                thenReturn(universityVSU);
+        when(passwordEncoder.encode("password")).
+                thenReturn("password");
+
+        when(userMapper.toEntity(newUserDto, universityVSU, group, facultyFKN, userToUpdate.getPassword())).
+                thenReturn(newUser);
+
+        userServiceImpl.updateUser(newUserDto, 3L);
+
+        assert(userToUpdate.getFullName().equals("Семёнов Артём Валерьевич"));
+        assert(userToUpdate.getEmail().equals("semen@mail.ru"));
+        assert(userToUpdate.getUsername().equals("semen"));
+        assert(userToUpdate.getFaculty().getName().equals("ФКН"));
     }
 
     @Test
@@ -140,7 +238,6 @@ class UserServiceImplTest {
         assertThat(user.getUsername()).isNotNull();
         assert(userToCompare.getUsername().equals("ivan"));
         assert(userToCompare.equals(user));
-
     }
 
     @Test
